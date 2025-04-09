a717593bd9902238cdddb13cee0f2ba8cae409fe
GeraldAkenji <akenjigerald@gmail.com>
2025-04-08 18:50:50 -0600

    deploy cloud load balancer

--- infra/main.tf
+++ infra/main.tf
@@ -75,3 +75,22 @@ resource "google_compute_url_map" "website" {
     default_service = google_compute_backend_bucket.website-backend.self_link
   }
 }
+
+# GCP target proxy
+resource "google_compute_target_https_proxy" "website" {
+  provider         = google
+  name             = "website-target-proxy"
+  url_map          = google_compute_url_map.website.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.website.self_link]
+}
+
+# GCP forwarding rule
+resource "google_compute_global_forwarding_rule" "default" {
+  provider              = google
+  name                  = "website-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address            = google_compute_global_address.website.address
+  ip_protocol           = "TCP"
+  port_range            = "443"
+  target                = google_compute_target_https_proxy.website.self_link
+}
